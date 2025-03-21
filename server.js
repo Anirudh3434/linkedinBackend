869ba8e37ed92405ab7c8c50508957461fb4d594
Anirudh <anigokala@outlook.com>
2025-03-21 18:22:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
+const REDIRECT_URI = 'https://linkedinbackend-ndvv.onrender.com/linkedin/callback';
 
 // ✅ LinkedIn JWKS client for token verification
 const client = jwksClient({
